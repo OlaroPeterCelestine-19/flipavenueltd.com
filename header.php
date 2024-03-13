0b45c9ceae3a6fede56c7ec1c997a20727d2ddcc
Oguti David Brian <39586345+osppro@users.noreply.github.com>
2024-03-13 17:06:35 +0300

    code
    Co-Authored-By: lastborn-19 <120270121+lastborn-19@users.noreply.github.com>

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Little Closet</title>
+	<title>flipavenueltd.com</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="description" content="Little Closet template">
@@ -13,9 +13,6 @@
 	<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
 	<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
 	<link rel="stylesheet" type="text/css" href="styles/responsive.css">
-	<script nonce="14bebb41-8921-427a-b4ce-668db0af2cbc">try{
-		(function(w,d){!function(du,dv,dw,dx){du[dw]=du[dw]||{};du[dw].executed=[];du.zaraz={deferred:[],listeners:[]};du.zaraz.q=[];du.zaraz._f=function(dy){return async function(){var dz=Array.prototype.slice.call(arguments);du.zaraz.q.push({m:dy,a:dz})}};for(const dA of["track","set","debug"])du.zaraz[dA]=du.zaraz._f(dA);du.zaraz.init=()=>{var dB=dv.getElementsByTagName(dx)[0],dC=dv.createElement(dx),dD=dv.getElementsByTagName("title")[0];dD&&(du[dw].t=dv.getElementsByTagName("title")[0].text);du[dw].x=Math.random();du[dw].w=du.screen.width;du[dw].h=du.screen.height;du[dw].j=du.innerHeight;du[dw].e=du.innerWidth;du[dw].l=du.location.href;du[dw].r=dv.referrer;du[dw].k=du.screen.colorDepth;du[dw].n=dv.characterSet;du[dw].o=(new Date).getTimezoneOffset();if(du.dataLayer)for(const dH of Object.entries(Object.entries(dataLayer).reduce(((dI,dJ)=>({...dI[1],...dJ[1]})),{})))zaraz.set(dH[0],dH[1],{scope:"page"});du[dw].q=[];for(;du.zaraz.q.length;){const dK=du.zaraz.q.shift();du[dw].q.push(dK)}dC.defer=!0;for(const dL of[localStorage,sessionStorage])Object.keys(dL||{}).filter((dN=>dN.startsWith("_zaraz_"))).forEach((dM=>{try{du[dw]["z_"+dM.slice(7)]=JSON.parse(dL.getItem(dM))}catch{du[dw]["z_"+dM.slice(7)]=dL.getItem(dM)}}));dC.referrerPolicy="origin";dC.src="../../cdn-cgi/zaraz/sd0d9.js?z="+btoa(encodeURIComponent(JSON.stringify(du[dw])));dB.parentNode.insertBefore(dC,dB)};["complete","interactive"].includes(dv.readyState)?zaraz.init():du.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document)}catch(e){throw fetch("/cdn-cgi/zaraz/t"),e;};
-	</script>
 </head>
 <body>
 <div class="menu">
@@ -58,13 +55,13 @@
 		<div class="logo">
 			<a href="">
 				<div class="d-flex flex-row align-items-center justify-content-start">
-				<div><img src="images/logo_1.png" alt></div>
-				<div>Little Closet</div>
+				<div><img src="uploads/logo.jpg" style="width: 40px;" alt></div>
+				<div>Flipavenuelid</div>
 				</div>
 			</a>
 		</div>
 		<div class="hamburger"><i class="fa fa-bars" aria-hidden="true"></i></div>
-		<nav class="main_nav">
+		<!-- <nav class="main_nav">
 			<ul class="d-flex flex-row align-items-start justify-content-start">
 				<li class="active"><a href="#">Women</a></li>
 				<li><a href="#">Men</a></li>
@@ -72,7 +69,7 @@
 				<li><a href="#">Home Deco</a></li>
 				<li><a href="#">Contact</a></li>
 			</ul>
-		</nav>
+		</nav> -->
 		<div class="header_right d-flex flex-row align-items-center justify-content-start ml-auto">
 			<div class="header_search">
 				<form action="#" id="header_search_form">
@@ -80,14 +77,14 @@
 					<button class="header_search_button"><img src="images/search.png" alt></button>
 				</form>
 			</div>
-			<div class="user">
+		<!-- 	<div class="user">
 				<a href="#">
 					<div>
 						<img src="images/user.svg" alt="https://www.flaticon.com/authors/freepik">
 						<div>1</div>
 					</div>
 				</a>
-			</div>
+			</div> -->
 			<div class="cart">
 				<a href="cart.html">
 					<div>
