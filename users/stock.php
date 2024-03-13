86afd60bf9a88331f83cb690cf7238b7b77c6f1a
Atugonza Benjamin Mukindo <153981563+atubenjan@users.noreply.github.com>
2024-03-13 06:34:36 -0700

    eee
    updates

--- users/stock.php
+++ users/stock.php
@@ -1,16 +1,7 @@
 <?php include 'header.php' ?>
 
 
-    <div class="pagetitle">
-      <h1>Data Tables</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item">Tables</li>
-          <li class="breadcrumb-item active">Data</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+   
 
     <section class="section">
       <div class="row">
