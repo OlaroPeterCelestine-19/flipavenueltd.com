ff3d36abbad4185f8020bc18ff231ef346d8a096
Atugonza Benjamin Mukindo <153981563+atubenjan@users.noreply.github.com>
2024-03-13 05:58:39 -0700

    update
    haeder update

--- users/header.php
+++ users/header.php
@@ -166,18 +166,6 @@
         </ul>
       </li><!-- End Forms Nav -->
 
-      <li class="nav-item">
-        <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-layout-text-window-reverse"></i><span>Products</span><i class="bi bi-chevron-down ms-auto"></i>
-        </a>
-        <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-          <li>
-            <a href="tables-general.html">
-              <i class="bi bi-circle"></i><span>Out Of Stock</span>
-            </a>
-          </li>
-       
-        </ul>
       </li><!-- End Tables Nav -->
 
 
